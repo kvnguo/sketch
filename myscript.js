33ca7390fb6b940aa3158f93476301af13ee94e9
kvnguo <kevinguo1011@gmail.com>
2023-07-19 12:23:38 -0500

    Added mouse events

--- myscript.js
+++ myscript.js
@@ -1,4 +1,5 @@
 const screen = document.querySelector(".container");
+const selected = document.querySelectorAll(".pixel");
 
 function createPixel(append, ...args) { 
     const pixel = document.createElement("div");
@@ -6,19 +7,44 @@ function createPixel(append, ...args) {
          pixel.classList.add(args[i]); 
     }
     append.appendChild(pixel); 
+    selectPixel(pixel); 
+    
 }
 
-function createCanvas(pixels) {
-    for(let i = 0; i < pixels; i++) {
+function selectPixel(item) {
+    let isMouseDown = false;
+
+    document.addEventListener("mousedown", () => {
+        isMouseDown = true;
+    });
+
+    document.addEventListener("mouseup", () => {
+        isMouseDown = false;
+    });
+
+    item.addEventListener("mousedown", ()=> {
+        item.classList.add("selected");
+        
+    }) 
+    item.addEventListener("mousemove", ()=> {
+        if(isMouseDown) {
+            item.classList.add("selected");
+        }
+    }) 
+}
+
+
+
+function createCanvas(xPixels, yPixels) {
+    for(let i = 0; i < yPixels; i++) {
         const columnId = "column-" + i;
         createPixel(screen, "column", columnId);
         const start = document.querySelector("." + columnId);
-        for(let j = 0; j < pixels; j++) {
+        for(let j = 0; j < xPixels; j++) {
             createPixel(start, "pixel");
         }
     }
 }
 
+createCanvas(64, 64); 
 
-
-createCanvas(64); 
